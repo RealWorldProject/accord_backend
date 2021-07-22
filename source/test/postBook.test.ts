a44d4f3452d49f80108b6b1156dc27444f0fba2e
asharyapandey <asharyap7@gmail.com>
2021-07-22 22:36:51 +0545

    Feature: All tests passed

--- source/test/postBook.test.ts
+++ source/test/postBook.test.ts
@@ -3,14 +3,19 @@ import app from "../app/app";
 import mongoose from "mongoose";
 
 beforeAll(async () => {
-    await mongoose.connect(
-        "mongodb://localhost:27017/application_development",
-        {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-        }
-    );
+    try {
+        await mongoose.connect(
+            "mongodb+srv://accord:@gileGroup1@accord.opzad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+            // "mongodb://localhost:27017/test",
+            {
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useUnifiedTopology: true,
+            }
+        );
+    } catch (error) {
+        console.log(error);
+    }
 });
 afterAll(async () => {
     // await mongoose.connection.dropDatabase();
@@ -18,39 +23,55 @@ afterAll(async () => {
 });
 
 const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZjk2ZGNlOGQxOTA1MjE2MDk5YjViMCIsImlhdCI6MTYyNjk2MzU4OX0.Rj81c0_HQsgpP-oX9aBOS6iuxJzyxE_8CUykN_WmNpU";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZjkxMzEwZDE1NzlhMDFmYzNjYTk3YiIsImlhdCI6MTYyNjkzNjA4MH0.sFLUJObq4MqGvowxIzob_lkWYkIn00xMCIv47mBPDo0";
 
 describe("APIs /bookPost", () => {
     describe("when user tries to post a book", () => {
-        test("should respond with a 201 status code", async () => {const response = await request(app)
+        test("should respond with a 201 status code", async () => {
+            const response = await request(app)
                 .post("/api/v1/book")
                 .set("authorization", "Bearer " + token)
                 .send({
-                    name: "Small Data",
+                    name: "Small Data Test",
                     price: 1200,
                     images: [
-                        "https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-e1464023124869.jpeg",
-                        "https://www.incimages.com/uploaded_files/image/1024x576/getty_883231284_200013331818843182490_335833.jpg",
+                        "https://upload.wikimedia.org/wikipedia/en/9/9c/Smalldatathetinycluesthatuncoverhugetrends.jpg",
                     ],
                     description: "The tiny clues that uncover huge trends",
                     author: "Martin Lindstrom",
-                    category: "60eee34135aece0f64ea5f69",
+                    category: "60f930a3d1579a01fc3ca985",
+                    isNew: true,
+                    isAvailableForExchange: true,
                 });
-            console.log(response);
             expect(response.statusCode).toBe(201);
         });
     });
 
-    describe("\nGET /book || When admin tries to verify book", () => {
-        test("should ")
-    })
-    
-
-    describe("\nGET /books/:categoryID || When user clicks on specific category in home_screen", () => {
-        test("should display the every books referenced to that category.", async () => {
-            
+    describe("\nPATCH /book/accept || When admin tries to verify book", () => {
+        test("should return 200 response", async () => {
             const response = await request(app)
-                .get("api/v1/books/60f930a3d1579a01fc3ca985")
+                .patch("/api/v1/book/accept/60f93a66d1579a01fc3ca99e")
+                .set("authorization", `Bearer ${token}`);
+            expect(response.statusCode).toBe(200);
+        });
+    });
+
+    describe("\nPATCH /book/reject || When admin tries to reject book", () => {
+        test("should return 200 response", async () => {
+            const response = await request(app)
+                .patch("/api/v1/book/reject/60f93a66d1579a01fc3ca99e")
+                .set("authorization", `Bearer ${token}`)
+                .send({ rejectionMessage: "This is for Testing" });
+
+            console.log(response.body);
+            expect(response.statusCode).toBe(200);
+        });
+    });
+
+    describe("\nGET /books || When user clicks on specific category in home_screen", () => {
+        test("should display the every books referenced to that category.", async () => {
+            const response = await request(app)
+                .get("/api/v1/books")
                 .set("authorization", `Bearer ${token}`);
             expect(response.statusCode).toBe(200);
         });
